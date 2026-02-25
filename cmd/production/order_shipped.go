62f4906b368b1a6548a8d76fad95b66a385551e1
vaziria <manorder123@gmail.com>
2026-02-25 16:27:11 +0700

    job shipped

--- cmd/production/order_shipped.go
+++ cmd/production/order_shipped.go
@@ -13,6 +13,9 @@ import (
 	"github.com/pdcgo/shared/custom_connect"
 	"github.com/pdcgo/shared/db_models"
 	"github.com/urfave/cli/v3"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
 	"gorm.io/gorm"
 )
 
@@ -25,6 +28,12 @@ func NewOrderShipped(
 	helper *Helper,
 ) OrderShippedFunc {
 	return func(ctx context.Context, c *cli.Command) error {
+		cancel, err := custom_connect.InitTracer("order-service-shipped-worker")
+		if err != nil {
+			return err
+		}
+
+		defer cancel(ctx)
 
 		orderService := order_ifaceconnect.NewOrderServiceClient(
 			http.DefaultClient,
@@ -35,7 +44,7 @@ func NewOrderShipped(
 		query := db.
 			Model(&db_models.Order{}).
 			Where("status = ?", db_models.OrdShipped).
-			Where("id > ?", 1037942).
+			Where("created_at > ?", time.Now().AddDate(0, 0, -15)).
 			Order("id asc")
 
 		rows, err := query.Rows()
@@ -49,12 +58,18 @@ func NewOrderShipped(
 			return err
 		}
 
+		// initiating tracert
+		tracer := otel.GetTracerProvider().Tracer("")
+
 		for rows.Next() {
 			var ord db_models.Order
 			err = db.ScanRows(rows, &ord)
 			if err != nil {
 				return err
 			}
+
+			var span trace.Span
+			ctx, span = tracer.Start(ctx, "check_shipped")
 
 			res, err := orderService.OrderTracking(ctx, &connect.Request[order_iface.OrderTrackingRequest]{
 				Msg: &order_iface.OrderTrackingRequest{
@@ -70,6 +85,9 @@ func NewOrderShipped(
 			})
 
 			if err != nil {
+				span.SetStatus(codes.Error, err.Error())
+				span.End()
+
 				slog.Error(err.Error())
 				time.Sleep(time.Second)
 				continue
@@ -78,6 +96,8 @@ func NewOrderShipped(
 			for _, item := range res.Msg.Result {
 				slog.Info("track", "order_id", ord.ID, "status", item.Status)
 			}
+
+			span.End()
 
 		}
 
